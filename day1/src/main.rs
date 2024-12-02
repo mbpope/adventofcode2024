66c6c838c24ce25bbba91a591c962e26f7739251
Micah Pope <mpope316@gmail.com>
2024-12-01 19:40:41 -0800

    Solve day 1 pts 1 & 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,20 +7,41 @@ fn main() {
     let mut locations_b: Vec<i32> = locations.iter().map(|x| i32::from_str(&x[8..13]).unwrap()).collect();
     locations_a.sort();
     locations_b.sort();
-
-    let mut res:i32 = 0;
-    for i in 0..locations.len() {
-        let diff = locations_a[i] - locations_b[i];
-        res = res + diff.abs();
-    }
-
-    println!("The grand total is: {}", res);
+    
+    part1(locations_a.clone(), locations_b.clone());
+    part2(locations_a.clone(), locations_b.clone());
 }
 
+fn part1(locations_a: Vec<i32>, locations_b: Vec<i32>) {
+    let mut res:i32 = 0;
+    for i in 0..locations_a.len() {
+        let diff = locations_a[i] - locations_b[i];
+        res += diff.abs();
+    }
+
+    println!("Pt. 1: {}", res);
+}
+
+fn part2(locations_a: Vec<i32>, locations_b: Vec<i32>) {
+    let loc_len = locations_a.len();
+    let mut sim_score = 0;
+    for i in 0..loc_len {
+        let mut num_similarities = 0;
+        for j in 0..loc_len {
+            if locations_a[i] == locations_b[j] {
+                num_similarities += 1;
+            }
+        }
+        sim_score += locations_a[i] * num_similarities;
+    }
+    println!("Pt. 2: {}", sim_score);
+}
+
+// shamelessly stolen from https://doc.rust-lang.org/stable/rust-by-example/std_misc/file/read_lines.html
 fn read_lines(filename: &str) -> Vec<String> {
     fs::read_to_string(filename) 
-        .unwrap()  // panic on possible file-reading errors
-        .lines()  // split the string into an iterator of string slices
-        .map(String::from)  // make each slice into a string
-        .collect()  // gather them together into a vector
+        .unwrap()  
+        .lines()  
+        .map(String::from)  
+        .collect()  
 }
